60f9f13275d05276ccefd4dcf278545eda0cf49a
apollee <mariabeatriz.dongamartins@gmail.com>
2026-01-16 13:07:37 +0000

    Re-added navigation bar to protected pages and switched to an horizontal one.

--- frontend/src/layouts/JournalLayout.tsx
+++ frontend/src/layouts/JournalLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "../components/Navigation";
+import "./JournalLayout.css"
 
 export default function JournalLayout() {
   return (
     <>
-      <main>
+     <Navigation />
+      <main className="page-content">
         <Outlet />
       </main>
     </>
