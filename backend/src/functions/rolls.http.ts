771cbd1d16a444d8428a75ce2222bb209065dc6d
apollee <mariabeatriz.dongamartins@gmail.com>
2026-01-29 17:43:36 +0000

    Added authentication to the backend API and code to extract the client principal

--- backend/src/functions/rolls.http.ts
+++ backend/src/functions/rolls.http.ts
@@ -1,11 +1,36 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { getRolls } from "../services/rolls.service";
 
+function getUserFromHeader(req: HttpRequest) {
+  const principal = req.headers.get("x-ms-client-principal");
+  if (!principal) return null;
+
+  const decoded = Buffer.from(principal, "base64").toString("utf-8");
+  return JSON.parse(decoded);
+}
+
 export async function getRollsHandler(req: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log("GET /rolls called");
   
+  const user = getUserFromHeader(req);
+
+  if (!user) {
+    return {
+      status: 401,
+      jsonBody: { message: "No user authenticated"},
+    };
+  }
+  const userId = user.userId;
+  context.log("User ID:", userId);
+
+  const rolls = getRolls();
+
   return {
-    jsonBody: getRolls(),
+    status: 200,
+    jsonBody: {
+      userId,
+      rolls,
+    },
   };
 }
 
